da2109381a235b627c3496f66bb9cfc50718008d
Shivam Patel <shivuptl839@gmail.com>
2023-07-11 14:45:06 -0400

    Reconstruct pdf component script improved

--- capstone-backend/Components/pdfComponent/reconstructPdf.py
+++ capstone-backend/Components/pdfComponent/reconstructPdf.py
@@ -4,23 +4,6 @@ from fpdf import FPDF
 import base64
 
 def reconstructPdf(bytes, json_data):
-    # try:
-    #     data = jsonData
-    #     image = data['image']
-    #     aiResponse = data['aiResponse']
-    #     page = data['page']
-    #     index = data['index']
-    # except Exception as e:
-    #     print("Invalidation inside JSON data")
-
-    # try:
-    #     with PdfWriter("AIResponse.pdf") as pdf:
-    #         # Need to design proper PDF below
-    #         for img in image:
-    #             pdf.save(img)
-    # except Exception as e:
-    #     print("PDF generation FAILED")
-
     # Sort the JSON data based on the "index" key
     sorted_json_data = sorted(json_data, key=lambda x: x['index'])
     # Create a new PDF object
@@ -37,50 +20,63 @@ def reconstructPdf(bytes, json_data):
     x = 10
     y_txt = 10
     y_img = 10
-    # Specify the maximum height of the image on a page
-    max_image_height = 100
-    
+
+    # Check is there more data or not
+    has_next_data = True
+
     # Iterate over the JSON data
-    for data in json_data:
+    for index, data in enumerate(json_data):
+        if index < len(json_data) - 1:
+            has_next_data = True
+        else:
+            has_next_data = False
+        
         # Iterate over the items in each dictionary
         pdf.set_y(y_txt)
         pdf.set_x(120)
         pdf.set_font("Arial", style="B")
         pdf.cell(0, 10, "Location", ln=True)
         y_txt += 10
+
         for key, value in data.items():
             if key == "image":
                 # Convert the base64 encoded image to bytes
                 image_data = base64.b64decode(value)
 
                 # Save the image to a file
-                image_file = f"temp_image_{data['index']}.png"
+                # image_file = f"temp_image_{data['index']}.png"
+
+                # Save the image to a file with the appropriate format
+                image_format = value.split(';')[0].split('/')[1]
+                image_file = f"temp_image_{data['index']}.{image_format}"
+
                 with open(image_file, 'wb') as file:
                     file.write(image_data)
                 
-                # Get the remaining space on the current page
-                remaining_height = pdf.h - pdf.get_y()
+                if is_image_format_supported(image_file) == False:
+                    img = Image.open(image_file)
+                    img = img.convert("RGBA")
+                    png_image_file = f"temp_image_{data['index']}.png"
+                    img.save(png_image_file, "PNG")
+                    image_file = png_image_file
+                    img.close()
 
-                # Get the height of the image
-                img = Image.open(image_file)
-                image_height = img.height
-                img.close()
-
-                # Check if the image fits on the current page
-                if image_height > remaining_height:
-                    pdf.add_page()
-                    y_img = 10  # Reset the y-coordinate for the new
-                    y_txt = 10
-                    
                 # Embed the image into the PDF
                 pdf.image(image_file, x=x, y=y_img, w=100, h=100)
 
                 # Update the position for the next image
-                y_img += 120
-                # y_txt += y_img - y_txt
-
-                # Remove the temporary image file
-                os.remove(image_file)
+                remaining_height = pdf.h - y_img - 100  # Calculate the remaining height after embedding the image
+                if has_next_data == True and remaining_height < 100:
+                    pdf.add_page()
+                    y_img = 10  # Reset the y-coordinate for the new
+                    y_txt = 10
+                else:
+                    y_img += 120
+                    y_txt += y_img - y_txt
+                
+                # Remove the temporary image files
+                os.remove(png_image_file)
+                os.remove(f"temp_image_{data['index']}.{image_format}")
             else:
                 pdf.set_y(y_txt)
                 pdf.set_x(125)  # Set the x position to 125 units
@@ -88,3 +84,10 @@ def reconstructPdf(bytes, json_data):
                 pdf.cell(10, 10, f"{key}: {value}", ln=True)
             y_txt += 10
 
+
+def is_image_format_supported(image_format):
+    supported_formats = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.wbmp']
+    if image_format.lower() in supported_formats:
+        return True
+    else:
+        return False
