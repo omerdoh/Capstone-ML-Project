9d484b42e89a12ba90d4f64ec8cc04adf6c090d4
kelseyjoshuaphillips <phil0382@algonquinlive.com>
2023-07-18 13:47:27 -0400

    Cleaned up ai-components and updated comments

--- capstone-backend/Components/ai-components.py
+++ capstone-backend/Components/ai-components.py
@@ -1,5 +1,3 @@
-#install version 2.8 of tensorflow
-#pip install tensorflow==2.8 tensorflow==2.8 opencv-python matplotlib protobuf==3.19.1
 import tensorflow as tf
 import numpy as np
 import cv2
@@ -7,17 +5,18 @@ from tensorflow.python.keras.models import load_model
 import base64
 import json
 
-#load the model
-#model = load_model("capstone-backend\ai-model\models\algonquinModel_1.h5")
+#load the models
 detectionModel = load_model("capstone-backend/ai-model/models/detectImagePeopleModel_V1.h5")
 logoModel = load_model("capstone-backend/ai-model/models/logoModel_V1.h5")
 peopleModel = load_model("capstone-backend/ai-model/models/peopleModel_V1.h5")
-
-#function that loads the model and calculates the weight from the model
-#jsonDoc is a json object
-#img is an encoded base64 string
-#returns an array with the weight and boolean 
-#in current model, less than 0.5 means off brand, greater than 0.5 means on brand
+"""
+Function that loads the model and calculates the weight from the model
+jsonDoc is a json object converted into a python dictionary
+jsonDoc['image'] is an encoded base64 string
+Returns a parseJson dictionary containing the ai-response
+ai-response[modelScore,good or bad value, corresponds to logo or person]
+In current model, less than 0.5 means off brand, greater than 0.5 means on brand
+"""
 def returnImageWeight(jsonDoc):
     response = []
     if(jsonDoc is None):
@@ -48,13 +47,11 @@ def returnImageWeight(jsonDoc):
             response = [weight,False,imageType]
         
         #_appendtoJson()
-        index['AIResponse'] = response
+        index['ai-response'] = response
     return parseJson
 
 
-
-
-#detect if image is of logos or people
+#Detects if image is of logos or people
 def _detectImageType(img):
     weightArray = detectionModel.predict(np.expand_dims(img/255,0))
     type = weightArray[0][0]
@@ -67,32 +64,10 @@ def _detectImageType(img):
         #it is a person
         return False
 
+#Converts the base64 image into cv2 format and resizes the image for the AI
 def _decodeResize(img):
     img = cv2.imdecode(np.frombuffer(base64.b64decode(img),dtype=np.uint8),cv2.IMREAD_COLOR)
     resize = tf.image.resize(img,(256,256))
     return resize
 
-def _appendToJson():
 
-    return
-
-"""
-file = open('capstone-backend/Components/pdfComponent/testjson.json')
-myJson = json.load(file)
-for brand in myJson:
-    
-    img = brand["image"]
-    resize = _decodeResize(img)
-    type = _detectImageType(resize)
-    #type is logo or people
-    imageType = 'logo'
-    #send to which model
-    modelResponse = 0.6
-    #response[weight,true/false]
-    response = [modelResponse,True,imageType]
-    #brand.append(response)
-    brand['airesponse'] = response
-    #add to myJson
-    #img.append(response)
-print(myJson)
-"""
